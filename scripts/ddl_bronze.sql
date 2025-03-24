4911757fdae7d46716abc8079bd38cea04e11c84
Conner Passafiume <87138328+ConnerPassa@users.noreply.github.com>
2025-03-24 02:37:19 -0400

    Update ddl_bronze.sql

--- scripts/ddl_bronze.sql
+++ scripts/ddl_bronze.sql
@@ -10,21 +10,21 @@ IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
 
 -- Create tables
 CREATE TABLE bronze.crm_cust_info (
-	cst_id				      INT,
-	cst_key				      NVARCHAR(50),
-	cst_firstname		    NVARCHAR(50),
-	cst_lastname		    NVARCHAR(50),
+	cst_id				INT,
+	cst_key				NVARCHAR(50),
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
 	cst_material_status	NVARCHAR(50),
-	cst_gndr			      NVARCHAR(50),
-	cst_create_date		  DATE
+	cst_gndr			NVARCHAR(50),
+	cst_create_date		DATE
 );
 
 CREATE TABLE bronze.crm_prd_info (
-	prd_id			  INT,
-	prd_key			  NVARCHAR(50),
-	prd_nm			  NVARCHAR(50),
-	prd_cost		  INT,
-	prd_line		  NVARCHAR(50),
+	prd_id			INT,
+	prd_key			NVARCHAR(50),
+	prd_nm			NVARCHAR(50),
+	prd_cost		INT,
+	prd_line		NVARCHAR(50),
 	prd_start_dt	DATETIME,
 	prd_end_dt		DATETIME
 );
